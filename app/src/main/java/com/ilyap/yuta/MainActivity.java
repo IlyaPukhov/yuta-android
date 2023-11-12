8e65f602654075397a531c4df14ec2f67c45becd
Ilya Pukhov <dinamond2003@gmail.com>
2023-11-12 22:56:19 +0300

    Add navigation bar and screen samples

--- app/src/main/java/com/ilyap/yuta/MainActivity.java
+++ app/src/main/java/com/ilyap/yuta/MainActivity.java
@@ -7,6 +7,11 @@ import android.content.SharedPreferences;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.NavigationUI;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,9 +20,14 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        SharedPreferences sharedPreferences = getSharedPreferences("session", Context.MODE_PRIVATE);
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigationView);
+        NavController navController = Navigation.findNavController(this, R.id.fragment);
+        NavigationUI.setupWithNavController(bottomNavigationView, navController);
 
-        // TODO Gson
-        String userJson = sharedPreferences.getString("user", EMPTY);
+        System.out.println("111");
+
+//        SharedPreferences sharedPreferences = getSharedPreferences("session", Context.MODE_PRIVATE);
+//        // TODO Gson
+//        String userJson = sharedPreferences.getString("user", EMPTY);
     }
 }
