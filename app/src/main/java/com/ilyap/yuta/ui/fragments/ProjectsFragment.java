feffbc78f744cd0b8d011effe37ee17bdb7432fb
Ilya Pukhov <dinamond2003@gmail.com>
2023-12-18 04:19:16 +0300

    Little code refactor

--- app/src/main/java/com/ilyap/yuta/ui/fragments/ProjectsFragment.java
+++ app/src/main/java/com/ilyap/yuta/ui/fragments/ProjectsFragment.java
@@ -1,9 +1,13 @@
 package com.ilyap.yuta.ui.fragments;
 
+import static com.ilyap.yuta.utils.UserUtils.logOut;
+
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ToggleButton;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
@@ -11,13 +15,64 @@ import androidx.fragment.app.Fragment;
 import com.ilyap.yuta.R;
 
 public class ProjectsFragment extends Fragment {
+    private ToggleButton managedProjectsButton;
+    private ToggleButton memberProjectsButton;
+    private View view;
+
+    private static int lastPickedButtonId;
 
     public ProjectsFragment() {
     }
 
     @Override
-    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_projects, container, false);
+        view = inflater.inflate(R.layout.fragment_projects, container, false);
+
+        projectsSwitchInitialize();
+
+        if (lastPickedButtonId == 0) {
+            lastPickedButtonId = memberProjectsButton.getId();
+        }
+
+        view.findViewById(R.id.log_out).setOnClickListener(v -> logOut(requireActivity()));
+        return view;
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+//        updateCarousels();
+    }
+
+
+    private void openTab(@NonNull Button button) {
+        button.performClick();
+    }
+
+    private void onToggleButtonClick(View view) {
+        final ToggleButton button = (ToggleButton) view;
+        final ToggleButton otherButton;
+
+        if (button.getId() == managedProjectsButton.getId()) {
+            otherButton = memberProjectsButton;
+//            fillCarousels(managedTeamsMembers);
+        } else {
+            otherButton = managedProjectsButton;
+//            fillCarousels(othersTeamsMembers);
+        }
+
+        button.setTextAppearance(R.style.active_toggle);
+        button.setChecked(true);
+        otherButton.setTextAppearance(R.style.default_toggle);
+        otherButton.setChecked(false);
+        lastPickedButtonId = button.getId();
+    }
+
+    private void projectsSwitchInitialize() {
+        managedProjectsButton = view.findViewById(R.id.manager_button);
+        memberProjectsButton = view.findViewById(R.id.member_button);
+        managedProjectsButton.setOnClickListener(this::onToggleButtonClick);
+        memberProjectsButton.setOnClickListener(this::onToggleButtonClick);
     }
 }
