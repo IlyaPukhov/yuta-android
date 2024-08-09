a4f8c719b9794e6786f63dd485f44c7b85bddd03
Ilya Pukhov <dinamond2003@gmail.com>
2024-08-09 13:16:40 +0300

    Authorization API service

--- data/src/main/java/com/yuta/data/network/service/AuthorizationApiService.kt
+++ data/src/main/java/com/yuta/data/network/service/AuthorizationApiService.kt
@@ -1,4 +1,16 @@
 package com.yuta.data.network.service
 
+import com.yuta.data.model.AuthorizationResponse
+import retrofit2.http.Field
+import retrofit2.http.FormUrlEncoded
+import retrofit2.http.POST
+
 interface AuthorizationApiService {
+
+    @POST("/api/authorization")
+    @FormUrlEncoded
+    suspend fun authorize(
+        @Field("login") login: String,
+        @Field("password") password: String
+    ): AuthorizationResponse
 }
