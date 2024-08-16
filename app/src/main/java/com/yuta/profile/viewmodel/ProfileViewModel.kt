e3de18100d9dc99fd67da8c630b10c097b14f503
Ilya Pukhov <dinamond2003@gmail.com>
2024-08-16 15:29:32 +0300

    ViewModel for profile

--- app/src/main/java/com/yuta/profile/viewmodel/ProfileViewModel.kt
+++ app/src/main/java/com/yuta/profile/viewmodel/ProfileViewModel.kt
@@ -1,12 +1,45 @@
 package com.yuta.profile.viewmodel
 
 import androidx.lifecycle.ViewModel
+import com.yuta.domain.model.User
+import com.yuta.domain.model.UserEditDto
+import com.yuta.domain.model.UserMiniatureUpdateDto
 import com.yuta.domain.usecase.ProfileUseCase
+import kotlinx.coroutines.flow.Flow
+import java.io.InputStream
 import javax.inject.Inject
 
 class ProfileViewModel @Inject constructor(
     private val useCase: ProfileUseCase
 ) : ViewModel() {
 
-    // TODO()
+    fun getProfile(userId: Int): Flow<User> {
+        return useCase.getUserById(userId)
+    }
+
+    fun syncProfile(userId: Int, password: String): Flow<Boolean> {
+        return useCase.syncUserData(userId, password)
+    }
+
+    fun editDetails(userId: Int, biography: String, phone: String, email: String, vk: String): Flow<Boolean> {
+        return useCase.editUser(UserEditDto(userId, biography, phone, email, vk))
+    }
+
+    fun updatePhoto(userId: Int, filename: String, photo: InputStream): Flow<Boolean> {
+        return useCase.updateUserPhoto(userId, filename, photo)
+    }
+
+    fun updateMiniature(
+        userId: Int, ivWidth: Int, ivHeight: Int,
+        croppedWidth: Int, croppedHeight: Int,
+        offsetX: Int, offsetY: Int
+    ): Flow<Boolean> {
+        return useCase.updateMiniatureUserPhoto(
+            UserMiniatureUpdateDto(userId, ivWidth, ivHeight, croppedWidth, croppedHeight, offsetX, offsetY)
+        )
+    }
+
+    fun deletePhoto(userId: Int): Flow<Boolean> {
+        return useCase.deleteUserPhoto(userId)
+    }
 }
